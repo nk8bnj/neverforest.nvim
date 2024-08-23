7a1f8210c94158f42be65e76f6e47dea47ae15e0
Nikita Ganzha <nk8bnj@gmail.com>
2024-08-23 23:42:21 +0300

    color updated

--- lua/lualine/themes/everforest.lua
+++ lua/lualine/themes/everforest.lua
@@ -20,27 +20,27 @@ return {
     c = {bg = palette.bg1[1], fg = palette.grey1[1]}
   },
   insert = {
-    a = {bg = palette.mainGrey[1], fg = palette.bg0[1], gui = 'bold'},
+    a = {bg = palette.aqua[1], fg = palette.bg0[1], gui = 'bold'},
     b = {bg = palette.bg3[1], fg = palette.fg[1]},
     c = {bg = palette.bg1[1], fg = palette.fg[1]}
   },
   visual = {
-    a = {bg = palette.mainGrey[1], fg = palette.bg0[1], gui = 'bold'},
+    a = {bg = palette.purple[1], fg = palette.bg0[1], gui = 'bold'},
     b = {bg = palette.bg3[1], fg = palette.fg[1]},
     c = {bg = palette.bg1[1], fg = palette.fg[1]}
   },
   replace = {
-    a = {bg = palette.orange[1], fg = palette.bg0[1], gui = 'bold'},
+    a = {bg = palette.red[1], fg = palette.bg0[1], gui = 'bold'},
     b = {bg = palette.bg3[1], fg = palette.fg[1]},
     c = {bg = palette.bg1[1], fg = palette.fg[1]}
   },
   command = {
-    a = {bg = palette.mainGrey[1], fg = palette.bg0[1], gui = 'bold'},
+    a = {bg = palette.orange[1], fg = palette.bg0[1], gui = 'bold'},
     b = {bg = palette.bg3[1], fg = palette.fg[1]},
     c = {bg = palette.bg1[1], fg = palette.fg[1]}
   },
   terminal = {
-    a = {bg = palette.mainGrey[1], fg = palette.bg0[1], gui = 'bold'},
+    a = {bg = palette.fg[1], fg = palette.bg0[1], gui = 'bold'},
     b = {bg = palette.bg3[1], fg = palette.fg[1]},
     c = {bg = palette.bg1[1], fg = palette.fg[1]}
   },
